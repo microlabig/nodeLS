b5be72cf058fa4646cc0a8cc17a9fe7e362b28c6
Igor Bezmestin <igor-rock@list.ru>
2020-03-14 16:26:08 +0300

    Сделано ДЗ №1

---
+++ main.js
@@ -0,0 +1,48 @@
+const fs = require('fs');
+const path = require('path');
+
+const FROM = process.argv[2] ? path.join(__dirname, 'src', process.argv[2]) : path.join(__dirname, 'src', './foods'); // исходный каталог
+const TO = process.argv[3] ? path.join(__dirname, 'src', process.argv[3]) : path.join(__dirname, 'src', './sort_foods'); // новый каталог
+
+// функция чтения исходного каталога и перемещения файлов
+const readDir = (scanPath) => {
+    const files = fs.readdirSync(scanPath);
+
+    files.forEach(file => {
+        const fullPath = path.join(path.resolve(scanPath), file);
+        const state = fs.statSync(fullPath);
+
+        if (state.isDirectory()) {
+            readDir(fullPath);
+        } else {
+            const firstLetter = file[0].toUpperCase();
+            const pathTo = path.join(TO, firstLetter);
+
+            // проверяем - существует ли такая папка с названием первой буквы в каталоге
+            if (!fs.existsSync(pathTo)) {
+                fs.mkdirSync(pathTo); // если нет - создать каталог
+            }
+            //переместим файл в папку с названием первой буквы в каталоге
+            fs.rename(fullPath, path.join(pathTo, file), (err) => errorMessage);
+        }
+    });
+}
+
+// функция обработки ошибок
+const errorMessage = (err) => {
+    if (err) {
+        console.error(err.message);
+        return;
+    }
+}
+
+// проверка существования выходного каталога
+if (!fs.existsSync(TO)) {
+    fs.mkdirSync(TO); // если нет - создать выходной каталог
+}
+// читаем каталог и перемещаем файлы
+readDir(FROM);
+// удаляем исходный каталог
+if (fs.existsSync(FROM)) {
+    fs.rmdir(FROM, { recursive: true }, (err) => errorMessage);
+}
