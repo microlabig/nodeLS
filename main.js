698ede39a0963e8b0f13d3c57bdf74f07f92d43c
Igor Bezmestin <igor-rock@list.ru>
2020-03-15 21:09:01 +0300

    Покоряю колбеки

--- main.js
+++ main.js
@@ -1,16 +1,22 @@
+/* eslint-disable */
 const fs = require('fs');
 const path = require('path');
+var argv = require('minimist')(process.argv.slice(2)); // читаем аргументы
+const folders = [...argv._]; // названия папок в src
 
-const FROM = process.argv[2]
-  ? path.join(__dirname, 'src', process.argv[2])
+const deleteSourceFolder = argv.d || false; // признак удаления исходной папки
+
+const FROM = folders[0]
+  ? path.join(__dirname, 'src', folders[0])
   : path.join(__dirname, 'src', './foods'); // исходный каталог
 
-const TO = process.argv[3]
-  ? path.join(__dirname, 'src', process.argv[3])
+const TO = folders[1]
+  ? path.join(__dirname, 'src', folders[1])
   : path.join(__dirname, 'src', './sort_foods'); // новый каталог
 
 // функция чтения исходного каталога и перемещения файлов
 const readDir = scanPath => {
+  
   const files = fs.readdirSync(scanPath);
 
   files.forEach(file => {
@@ -22,13 +28,28 @@ const readDir = scanPath => {
     } else {
       const firstLetter = file[0].toUpperCase();
       const pathTo = path.join(TO, firstLetter);
+      const newPath = path.join(pathTo, file);
 
       // проверяем - существует ли такая папка с названием первой буквы в каталоге
       if (!fs.existsSync(pathTo)) {
         fs.mkdirSync(pathTo); // если нет - создать каталог
       }
+      
       // переместим файл в папку с названием первой буквы в каталоге
-      fs.rename(fullPath, path.join(pathTo, file), err => errorMessage(err));
+      fs.rename(fullPath, newPath, err => {
+        errorMessage(err);
+        //const state = fs.statSync(FROM);
+        const files = fs.readdirSync(scanPath);
+
+        console.log(files);
+        
+        // удаляем исходный каталог по признаку --d
+        // if (deleteSourceFolder) {
+        //   if (fs.existsSync(scanPath) && files.length === 0) {
+        //     fs.rmdir(scanPath, err => errorMessage(err));
+        //   }
+        // }
+      });
     }
   });
 };
@@ -47,7 +68,3 @@ if (!fs.existsSync(TO)) {
 }
 // читаем каталог и перемещаем файлы
 readDir(FROM);
-// удаляем исходный каталог
-if (fs.existsSync(FROM)) {
-  fs.rmdir(FROM, { recursive: true }, err => errorMessage(err));
-}
