15a038f65525f2300157e74db3f800ed5db2e4b5
Igor Bezmestin <igor-rock@list.ru>
2020-03-24 18:34:18 +0300

    Переделал задание с сервером

--- main3.js
+++ main3.js
@@ -7,39 +7,14 @@ const PORT = process.env.PORT
   ? parseInt(process.env.PORT)
   : parseInt(config.parsed.PORT);
 // задержка между тиками (в мс)
+const INTERVAL = process.env.INTERVAL
+  ? parseInt(process.env.INTERVAL)
+  : parseInt(config.parsed.INTERVAL);
+// лимит таймера (количество тиков)
 const DELAY = process.env.DELAY
   ? parseInt(process.env.DELAY)
   : parseInt(config.parsed.DELAY);
-// лимит таймера (количество тиков)
-const TIMELIMIT = process.env.TIMELIMIT
-  ? parseInt(process.env.TIMELIMIT)
-  : parseInt(config.parsed.TIMELIMIT);
-
-let connections = []; // список подключившихся
-
-// -----------------------------------------------
-// Функция возвращает текущую дату в формате стр.
-// -----------------------------------------------
-function getCurrentDateTime () {
-  const currDate = new Date(); // текущая дата
-
-  let month = currDate.getMonth() + 1; // т.к. январь - 1, не 0!
-  let day = currDate.getDate();
-  let hours = currDate.getHours();
-  let minutes = currDate.getMinutes();
-  let seconds = currDate.getSeconds();
-
-  // сформируем двузначные значения
-  month = month < 10 ? '0' + month : '' + month;
-  day = day < 10 ? '0' + day : '' + day;
-  hours = hours < 10 ? '0' + hours : '' + hours;
-  minutes = minutes < 10 ? '0' + minutes : '' + minutes;
-  seconds = seconds < 10 ? '0' + seconds : '' + seconds;
-
-  // сформируем текущую дату в нужный формат по UTC
-  return `${day}.${month}.${currDate.getFullYear()}, ${hours.toUpperCase()}:${minutes}:${seconds}`;
-}
-
+  
 // ---------------
 // Создаем сервер
 // ---------------
@@ -64,24 +39,19 @@ server.on('request', (req, res) => {
   const method = req.method;
 
   if (method === 'GET' && isOnlyHtmlRequest !== -1) {
-    let tick = 0;
+      let date = new Date().toUTCString();
 
-    connections.push(res);
-    setTimeout(function run () {
-      const date = getCurrentDateTime();
-      console.log(date);
-      if (++tick > TIMELIMIT) {
-        connections.forEach((res, i) => {
-          res.write(date);
-          res.end();
-          console.log(`${i}'nth is closed`);
-        });
-        connections = [];
-        tick = 0;
-      } else {
-        setTimeout(run, DELAY);
-      }
-    }, DELAY);
+      const timer = setInterval(() => {
+        date = new Date().toUTCString();
+        console.log(date);
+      }, INTERVAL);
+
+      setTimeout(() => {
+        clearInterval(timer);
+        res.write(date);
+        console.log('close');
+        res.end();
+      }, DELAY);
   }
 });
 
