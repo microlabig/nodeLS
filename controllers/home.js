68004730638cb620d122945553289b3905c311a5
Igor Bezmestin <igor-rock@list.ru>
2020-04-10 21:16:07 +0300

    Создал проект, залил билд, спроектировал роутер

--- controllers/home.js
+++ controllers/home.js
@@ -1,29 +1,89 @@
-const db = require('../store');
-const { getSkills } = require('../store/skills');
-const { getProducts } = require('../store/products');
+// const db = require('../store');
+// const { getSkills } = require('../store/skills');
+// const { getProducts } = require('../store/products');
 
-module.exports.get = async (ctx, next) => {
+module.exports.get = (req, res) => {
   // req.session.isAuth = false;
-  return await ctx.render('index', {
-    products: getProducts(db),
-    skills: getSkills(db)
-  });
+  const url = req.url;
+
+  switch (url) {
+    case '/api/profile':
+      break;
+    case '/api/news':
+      break;
+    case '/api/users':
+      break;
+    default:
+      res.redirect('/');
+      break;
+  }
 };
 
-module.exports.post = async (ctx, next) => {
-  const { name, email, message } = ctx.request.body;
-  const isValid = !name || !email || !message;
-  const flashMessage = isValid
-    ? 'Ошибка ввода данных! Все поля обязательны для заполнения!'
-    : 'Форма отправлена успешно!';
+module.exports.post = (req, res) => {
+  const url = req.url;
 
-  // инициализируем flash-сообщение
-  ctx.flash('msgsemail', flashMessage);
-  // рендерим страницу с flash-сообщением
-  console.log(ctx.request.body);
-  return await ctx.render('index', {
-    products: getProducts(db),
-    skills: getSkills(db),
-    msgsemail: ctx.flash('msgsemail')
-  });
+  switch (url) {
+    case '/api/registration':
+      break;
+    case '/api/login':
+      break;
+    case '/api/refresh-token':
+      break;
+    case '/api/news':
+      break;
+
+    default:
+      res.send('Error');
+      break;
+  }
 };
+
+module.exports.patch = (req, res) => {
+  const url = req.url;
+
+  switch (url) {
+    case '/api/profile':
+      break;
+    case '/api/news/:id':
+      break;
+    case '/api/users/:id/permission':
+      break;
+
+    default:
+      res.send('Error');
+      break;
+  }
+};
+
+module.exports.delete = (req, res) => {
+  const url = req.url;
+
+  switch (url) {
+    case '/api/users/:id':
+      break;
+    case '/api/news/:id':
+      break;
+
+    default:
+      res.send('Error');
+      break;
+  }
+};
+
+// module.exports.post = (req, res) => {
+//   const { name, email, message } = req.body;
+//   const isValid = !name || !email || !message;
+//   const flashMessage = isValid
+//     ? 'Ошибка ввода данных! Все поля обязательны для заполнения!'
+//     : 'Форма отправлена успешно!';
+
+//   // инициализируем flash-сообщение
+//   req.flash('msgsemail', flashMessage);
+//   // рендерим страницу с flash-сообщением
+//   res.render('index', {
+//     products: getProducts(db),
+//     skills: getSkills(db),
+//     msgsemail: req.flash('msgsemail')
+//   });
+//   console.log(req.body);
+// };
--- routes/index.js
+++ routes/index.js
@@ -1,40 +1,29 @@
-const Router = require('koa-router');
-const router = new Router();
+const express = require('express');
+const router = express.Router();
 
 const ctrlHome = require('../controllers/home');
-const ctrlLogin = require('../controllers/login');
-const ctrlAdmin = require('../controllers/admin');
+// const ctrlLogin = require('../controllers/login');
+// const ctrlAdmin = require('../controllers/admin');
 
 // MW
-const isAuth = async (ctx, next) => {
-  // если в сессии текущего пользователя есть пометка о том, что он является администратором
-  // иначе перебросить пользователя на главную страницу сайта
-  return ctx.session.isAuth ? next() : ctx.redirect('/login');
-};
+// const isAuth = (req, res, next) => {
+//   // если в сессии текущего пользователя есть пометка о том, что он является администратором
+//   // иначе перебросить пользователя на главную страницу сайта
+//   return req.session.isAuth ? next() : res.redirect('/login');
+// };
 
-// MW для лога url
-router.get(/.*/, async (ctx, next) => {
-  console.log(`Request: ${ctx.request.method}, ${ctx.request.url}`);
-  await next();
-});
+router.get(/.*$/, ctrlHome.get);
+router.post(/.*$/, ctrlHome.post);
+router.patch(/.*$/, ctrlHome.patch);
+router.delete('/api/users/:id', ctrlHome.delete);
 
-router.get('/', ctrlHome.get);
-router.post('/', ctrlHome.post);
+// router.get('/login', ctrlLogin.get);
+// router.get('/login(.html?)?$/', (req, res) => res.redirect('/login'));
+// router.post('/login', ctrlLogin.post);
 
-router.get('/login', ctrlLogin.get);
-router.get(
-  /login(\.html?)?$/,
-  async (ctx, next) => ctx.redirect('/login')
-);
-router.post('/login', ctrlLogin.post);
-
-router.get('/admin', isAuth, ctrlAdmin.get);
-router.get(
-  /admin(\.html?)?$/,
-  isAuth,
-  async (ctx, next) => ctx.redirect('/admin')
-);
-router.post('/admin/skills', ctrlAdmin.skills);
-router.post('/admin/upload', ctrlAdmin.product);
+// router.get('/admin', isAuth, ctrlAdmin.get);
+// router.get('/admin(.html?)?$/', isAuth, (req, res) => res.redirect('/admin'));
+// router.post('/admin/skills', ctrlAdmin.skills);
+// router.post('/admin/upload', ctrlAdmin.product);
 
 module.exports = router;
