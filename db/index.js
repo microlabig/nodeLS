35a545a00287e003dfde7e358b9ea6d9c16d9556
Igor Bezmestin <igor-rock@list.ru>
2020-05-15 23:56:59 +0300

    Поправил настройки БД 2

--- db/index.js
+++ db/index.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const LOCAL_PATH = `mongodb://${process.env.DB_HOST}:${process.env.DB_PORT}/loft`;
 const REMOTE_PASS = encodeURIComponent(process.env.REMOTE_DB_PASS);
 const REMOTE_PATH = `mongodb+srv://${process.env.REMOTE_DB_USER}:${REMOTE_PASS}@cluster0-rktdm.mongodb.net/${process.env.REMOTE_DB_NAME}?retryWrites=true&w=majority`;
+console.log('---',process.env.REMOTE_DB_USER, process.env.REMOTE_DB_PASS);
 
 const mode = process.env.mode;
 const dbPath = mode && mode === 'development' ? LOCAL_PATH : REMOTE_PATH;
@@ -13,7 +14,7 @@ const NewsDB = require('./news');
 // соединяемся с БД
 mongoose
   .connect(dbPath, {
-    useNewUrlParser: true
+    useNewUrlParser: true,
     // useUnifiedTopology: true
   })
   .then(() => {
