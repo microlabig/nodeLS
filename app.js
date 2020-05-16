b2f97944602595d5d70f8dea67a6b2dcb5f5e78c
Igor Bezmestin <igor-rock@list.ru>
2020-05-16 14:21:49 +0300

    Установка cors

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const PORT = process.env.PORT || 3000; // порт сервера
 const path = require('path');
 const bodyParser = require('body-parser');
 const session = require('express-session');
+const cors = require('cors');
 const express = require('express');
 const app = express();
 const socketRun = require('./chat');
@@ -30,21 +31,22 @@ app.use(
 app.use(express.static(path.join(__dirname, 'public')));
 
 // роутер
-const allowCrossDomain = (req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Content-Type, Authorization, Content-Length, X-Requested-With'
-  );
-  // intercept OPTIONS method
-  if (req.method === 'OPTIONS') {
-    res.send(200);
-  } else {
-    next();
-  }
-};
-app.use(allowCrossDomain);
+// const allowCrossDomain = (req, res, next) => {
+//   res.header('Access-Control-Allow-Origin', '*');
+//   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+//   res.header(
+//     'Access-Control-Allow-Headers',
+//     'Content-Type, Authorization, Content-Length, X-Requested-With'
+//   );
+//   // intercept OPTIONS method
+//   if (req.method === 'OPTIONS') {
+//     res.send(200);
+//   } else {
+//     next();
+//   }
+// };
+// app.use(allowCrossDomain);
+app.use(cors());
 app.use('/', require('./routes'));
 
 // сокет на socket.io (чат)
